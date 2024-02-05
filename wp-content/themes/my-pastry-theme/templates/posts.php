0e2c3752f9e427945ef1e2c3672a862ad8d10dbd
Lao Oceane <oceane.lao1@gmail.com>
2024-02-05 01:04:50 +0100

    correct posts & add screenshot.png

--- wp-content/themes/my-pastry-theme/templates/posts.php
+++ wp-content/themes/my-pastry-theme/templates/posts.php
@@ -7,9 +7,6 @@ if (have_posts()) : while (have_posts()) : the_post();
     <p>
         <?php the_content(); ?>
     </p>
-    <p>
-        <?php the_content(); ?>
-    </p>
 <?php
     endwhile;
 endif;
